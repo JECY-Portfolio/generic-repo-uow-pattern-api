fc38d9c15cca82f52da65ee1ac8efbce15b69374
Onyedikachi Geoffrey <jecintageoffrey@yahoo.com>
2025-10-23 18:38:55 +0100

    Added IUnitOfWork interface and UnitOfWork implementation to manage multiple repositories and ensure transactional consistency

---
+++ generic-repo-uow-pattern-api/Repository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace generic_repo_pattern_api.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<T> GetRepository<T>() where T : class;
+        Task<int> SaveChangesAsync();
+        Task BeginTransactionAsync();
+        Task CommitAsync();
+        Task RollbackAsync();
+    }
+}
